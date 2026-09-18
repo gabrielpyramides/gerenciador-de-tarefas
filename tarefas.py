ad20fe973241b8529c154021aaa7110ef4988004
Gabriel Pyrâmides <gabrielcandido1211@gmail.com>
2026-09-18 17:51:19 -0300

    listando tarefas

--- tarefas.py
+++ tarefas.py
@@ -3,18 +3,21 @@ lista_tarefas = []
 
 def criar_tarefa():
     while True:
-        nome_tarefa = input('Digite a tarefa que gostaria de adicionar: ')
+        nome_tarefa = str(input('Digite a tarefa que gostaria de adicionar: '))
         concluida = False
-        lista_tarefas.append([{'Título': nome_tarefa}, {'concluida?': concluida}])
+        lista_tarefas.append({'Título': nome_tarefa, 'concluida?': concluida})
         adicionar_novamente = input('Deseja adicionar mais uma tarefa?(s/n)')
         if adicionar_novamente.lower().strip() == 's':
             continue
         else:
             return lista_tarefas
 
-def listar_tarefas():
-    pass
-
+def listar_tarefas(tarefas):
+    for indice, tarefa in enumerate(tarefas, start=1):
+        concluido = 'Concluída' if tarefa['concluida?'] else 'Não Realizada'
+        print(f'{indice}. Título: {tarefa['Título']} | Status: {concluido} ')
+    
+  
 def editar_tarefa():
     pass
 
@@ -22,4 +25,5 @@ def excluir_tarefa():
     pass
 
 criar_tarefa()
-print(lista_tarefas)
+listar_tarefas(lista_tarefas)
+
